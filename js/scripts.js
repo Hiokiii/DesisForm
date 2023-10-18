6c96ddc7fb570c6c1b759a0f8f360c4cd566532a
Hiokiii <hioki96@hotmail.com>
2023-10-18 03:49:14 -0300

    Validacion Rut

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,26 @@ const region = document.getElementById('region').value;
 const comuna = document.getElementById('comuna').value;
 const candidato = document.getElementById('candidato').value;
 const errorElement = document.getElementById('error')
+const Fn = {
+    // Valida el rut con su cadena completa "XXXXXXXX-X"
+    validaRut : function (rutCompleto) {
+        rutCompleto = rutCompleto.replace("‐","-");
+        if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
+            return false;
+        var tmp     = rutCompleto.split('-');
+        var digv    = tmp[1]; 
+        var rut     = tmp[0];
+        if ( digv == 'K' ) digv = 'k' ;
+        
+        return (Fn.dv(rut) == digv );
+    },
+    dv : function(T){
+        var M=0,S=1;
+        for(;T;T=Math.floor(T/10))
+            S=(S+T%10*(9-M++%6))%11;
+        return S?S-1:'k';
+    }
+}
 
 
 votingForm.addEventListener('submit', (e) => {
@@ -17,13 +37,17 @@ votingForm.addEventListener('submit', (e) => {
     if (alias.value.length<= 5 || !aliasRegex.test(alias)){
         messages.push('El campo Alias debe tener más de 5 caracteres y contener letras y números.')
     }
-    //falta rut
+    
+    // Validación de RUT
+    if (!Fn.validaRut(rut.value)) {
+        messages.push('El RUT ingresado no es válido.')
+    }
 
     if (!emailRegex.test(email)) {
         messages.push('El correo electrónico ingresado no es válido.')
     }
 
-    // Validar region comuna
+    // Validar region comunaquie
     
     if (region.value === '' || region.value == null || comuna.value === '' || comuna.value == null) {
         messages.push('Los campos Region y Comuna no deben quedar en blanco.')
@@ -45,3 +69,10 @@ votingForm.addEventListener('submit', (e) => {
         errorElement.innerText = messages.join(', ')
     }
 })
+
+
+//Falta
+//Pasar votacion a base de datos
+// Revisar duplicacion de rut
+//Mejorar Mensaje de cada error 
+//Mejorar ingreso de rut al dejar de tener seleccionado el input
